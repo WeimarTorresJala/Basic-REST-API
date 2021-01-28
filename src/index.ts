411ef8892b73c04f2898ee73193dc6cfc630dcd5
WeimarTorres <alexandertorreshe@gmail.com>
2021-01-27 22:46:13 -0400

    First commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import express = require("express");
+
+const app: express.Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from express and typescript')
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`App listening on PORT ${port}`));
