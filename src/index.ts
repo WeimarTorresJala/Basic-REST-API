f604384fcad8888c8357f01981b702c973365d6e
WeimarTorresJala <Weimar.Torres@jala-foundation.org>
2021-01-29 12:38:45 -0400

    Test 1

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express = require("express");
 const app: express.Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from express and typescript')
+    res.send('Hello from express and typescript 1')
 });
 
 const port = process.env.PORT || 3000;
